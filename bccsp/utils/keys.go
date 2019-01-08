dc9168f01e00311400247abd4a0e32200a2f59ed
zhangqichao <zhangqichao@gjk365.com>
2019-01-08 18:05:38 +0800

    fix key identity bug

--- bccsp/utils/keys.go
+++ bccsp/utils/keys.go
@@ -214,6 +214,10 @@ func DERToPrivateKey(der []byte) (key interface{}, err error) {
 		return
 	}
 
+	if key, err := sm2.ParsePKCS8UnecryptedPrivateKey(der); err == nil {
+		return key, nil
+	}
+
 	return nil, errors.New("Invalid key type. The DER must contain an rsa.PrivateKey or ecdsa.PrivateKey")
 }
 
