285152cc99864bed9076a42356168e9ba53a9cb4
zhangqichao <zhangqichao@gjk365.com>
2019-04-04 10:33:27 +0800

    add logs again

--- msp/mspimplvalidate.go
+++ msp/mspimplvalidate.go
@@ -199,7 +199,7 @@ func (msp *bccspmsp) validateIdentityOUsV11(id *identity) error {
 		}
 	}
 	if counter != 1 {
-		return errors.Errorf("the identity must be a client, a peer or an orderer identity to be valid, not a combination of them. OUs: [%v], MSP: [%s] OU.OrganizationalUnitIdentifier", id.GetOrganizationalUnits(), msp.name, id.GetOrganizationalUnits()[0].OrganizationalUnitIdentifier)
+		return errors.Errorf("the identity must be a client, a peer or an orderer identity to be valid, not a combination of them. OUs: [%v], MSP: [%s] OU.OrganizationalUnitIdentifier %v", id.GetOrganizationalUnits(), msp.name, id.GetOrganizationalUnits()[0].OrganizationalUnitIdentifier)
 	}
 
 	return nil
